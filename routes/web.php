6e0584ef91cf9404d8d7c41abf52896535978db5
Mamat1411 <muhammadmujahid14@gmail.com>
2022-07-15 11:14:59 +0700

    Creating home, about and posts page and their routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/posts', function () {
+    return view('posts');
+});
