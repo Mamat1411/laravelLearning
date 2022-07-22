07eeee8b24fcd1cb5f6c8bac245f251dd1bc72ea
Mamat1411 <muhammadmujahid14@gmail.com>
2022-07-22 09:47:10 +0700

    Generating Main Layout and Implementing Blade to the Existing Pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>About Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>About Page</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-  </body>
-</html>
+@extends('layout/main')
+@section('title', 'About Page')
+
+@section('container')
+    <div class="container">
+        <div class="column">
+            <div class="row">
+                About Page
+            </div>
+        </div>
+    </div>
+@endsection
