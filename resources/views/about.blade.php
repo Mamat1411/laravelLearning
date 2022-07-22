805c429392b0fd8493a6a78e0bdd4ffb29d12fde
Mamat1411 <muhammadmujahid14@gmail.com>
2022-07-22 14:12:00 +0700

    About Page Image Rounded Pill

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,5 @@
 @section('container')
     <h2>{{ $name }}</h1>
     <h3>{{ $email }}</h3>
-    <img src="{{ $image }}" alt="Mamat" width="200px">
+    <img src="{{ $image }}" alt="Mamat" width="200px" class="img-thumbnail rounded-pill">
 @endsection
