0d2b73d75d96fc54c667fd63dedf9b1e7f103ae6
Mamat1411 <muhammadmujahid14@gmail.com>
2022-07-22 11:14:02 +0700

    Generating a Single Post Page using Slug

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,13 @@ Route::get('/posts', function () {
     $blog_posts = [
         [
             "title" => "First Post Title",
+            "slug" => "first-post-title",
             "author" => "Mamat",
             "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, non ipsum inventore magnam praesentium autem omnis in, alias vitae perferendis saepe quia quisquam eveniet necessitatibus corrupti. Ipsum sit ex, sed repellat expedita voluptatibus quo labore asperiores perspiciatis natus nesciunt quis, assumenda impedit modi nobis. Dignissimos laudantium rerum distinctio laboriosam quaerat voluptatibus nihil perferendis aperiam assumenda earum incidunt vero corrupti, reiciendis tenetur iusto asperiores ipsa velit ex illum possimus omnis repellendus cum repudiandae ad! Vero omnis vitae deserunt quo excepturi illum?"
         ],
         [
             "title" => "Second Post Title",
+            "slug" => "second-post-title",
             "author" => "Muhammad",
             "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, non ipsum inventore magnam praesentium autem omnis in, alias vitae perferendis saepe quia quisquam eveniet necessitatibus corrupti. Ipsum sit ex, sed repellat expedita voluptatibus quo labore asperiores perspiciatis natus nesciunt quis, assumenda impedit modi nobis. Dignissimos laudantium rerum distinctio laboriosam quaerat voluptatibus nihil perferendis aperiam assumenda earum incidunt vero corrupti, reiciendis tenetur iusto asperiores ipsa velit ex illum possimus omnis repellendus cum repudiandae ad! Vero omnis vitae deserunt quo excepturi illum?"
         ]
@@ -44,5 +46,33 @@ Route::get('/posts', function () {
     return view('posts', [
         "title" => "Posts",
         "posts" => $blog_posts
+    ]);
+});
+
+//single post page
+Route::get('/posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "First Post Title",
+            "slug" => "first-post-title",
+            "author" => "Mamat",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, non ipsum inventore magnam praesentium autem omnis in, alias vitae perferendis saepe quia quisquam eveniet necessitatibus corrupti. Ipsum sit ex, sed repellat expedita voluptatibus quo labore asperiores perspiciatis natus nesciunt quis, assumenda impedit modi nobis. Dignissimos laudantium rerum distinctio laboriosam quaerat voluptatibus nihil perferendis aperiam assumenda earum incidunt vero corrupti, reiciendis tenetur iusto asperiores ipsa velit ex illum possimus omnis repellendus cum repudiandae ad! Vero omnis vitae deserunt quo excepturi illum?"
+        ],
+        [
+            "title" => "Second Post Title",
+            "slug" => "second-post-title",
+            "author" => "Muhammad",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, non ipsum inventore magnam praesentium autem omnis in, alias vitae perferendis saepe quia quisquam eveniet necessitatibus corrupti. Ipsum sit ex, sed repellat expedita voluptatibus quo labore asperiores perspiciatis natus nesciunt quis, assumenda impedit modi nobis. Dignissimos laudantium rerum distinctio laboriosam quaerat voluptatibus nihil perferendis aperiam assumenda earum incidunt vero corrupti, reiciendis tenetur iusto asperiores ipsa velit ex illum possimus omnis repellendus cum repudiandae ad! Vero omnis vitae deserunt quo excepturi illum?"
+        ]
+    ];
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
     ]);
 });
