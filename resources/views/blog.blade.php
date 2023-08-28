39da093c1ffecf51140d6fe7fa00e111af81c8ee
Mamat1411 <muhammadmujahid14@gmail.com>
2023-08-28 16:23:48 +0700

    Generating All Posts and Single Post View

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,31 +1,13 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mamat Blog | Blog Posts</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  </head>
-  <body>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container">
-            <a class="navbar-brand" href="#">Mamat Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div class="container mt-3">
-            <h1>Hello, world!</h1>
-        </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-  </body>
-</html>
+@extends('layouts.main')
+@section('title', 'Home')
+@section('container')
+    @foreach ($posts as $post)
+        <article class="mb-5">
+            <a href="/posts/{{ $post["slug"] }}" class="text-decoration-none">
+                <h2>{{ $post["title"] }}</h2>
+            </a>
+            <h5>By: {{ $post["author"] }}</h5>
+            <p>{{ $post["body"] }}</p>
+        </article>
+    @endforeach
+@endsection
---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('container')
+    <article>
+        <h2>{{ $post["title"] }}</h2>
+        <h5>By: {{ $post["author"] }}</h5>
+        <p>{{ $post["body"] }}</p>
+    </article>
+    <a href="/" class="text-decoration-none"> Back to Posts</a>
+@endsection
