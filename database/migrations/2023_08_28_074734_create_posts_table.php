8f5a5f1b9874155925778dd440d212b40e5e123d
Mamat1411 <muhammadmujahid14@gmail.com>
2023-09-05 07:40:39 +0700

    Add Unique Constraint on slug field

--- database/migrations/2023_08_28_074734_create_posts_table.php
+++ database/migrations/2023_08_28_074734_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id');
             $table->string('title');
             $table->text('excerpt');
-            $table->string('slug', 100);
+            $table->string('slug', 100)->unique();
             $table->text('body');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
