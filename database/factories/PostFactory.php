18200841532d550a9685049e5669d498b6639f1e
Mamat1411 <muhammadmujahid14@gmail.com>
2023-09-05 07:44:00 +0700

    Post Factory Using Faker

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(mt_rand(2,8), false),
+            'category_id' => 1,
+            'user_id' => 1,
+            'slug' => fake()->slug(mt_rand(2,8)),
+            'excerpt' => fake()->sentence(mt_rand(2,8), false),
+            'body' => fake()->paragraphs(mt_rand(3,8), true)
+        ];
+    }
+}
