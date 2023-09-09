0cbd288c55673cb5b638fade65850583a78abf2b
Mamat1411 <muhammadmujahid14@gmail.com>
2023-09-09 14:19:24 +0700

    Display unescaped data

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,8 @@
 @section('container')
     <article class="mb-4">
         <h2>{{ $post->title }}</h2>
-        <h5>By: <a href="" class="text-decoration-none">{{ decrypt($post->user->name) }}</a> in <a href="/categories/{{ $post->category->name }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
-        {{ $post->body }}
+        <h5 class="mb-5">By: <a href="" class="text-decoration-none">{{ decrypt($post->user->name) }}</a> in <a href="/categories/{{ $post->category->name }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+        {!! $post->body !!}
     </article>
     <a href="/" class="text-decoration-none"> << Back to Posts</a>
 @endsection
